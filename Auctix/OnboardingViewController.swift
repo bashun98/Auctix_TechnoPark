f7d040f3f54f3d20d6bf7ea54f34e26e50d6983d
bashun98 <bashun98@gmail.com>
2021-10-28 14:49:26 +0300

    add onboarding

---
+++ Auctix/OnboardingViewController.swift
@@ -0,0 +1,123 @@
+//
+//  OnboardingViewController.swift
+//  Auctix
+//
+//  Created by Евгений Башун on 28.10.2021.
+//
+
+import UIKit
+import SnapKit
+
+final class OnboardingViewController: UIViewController {
+
+    private let label = UILabel()
+    private let extralabel = UILabel()
+    private let buttonContinue = UIButton()
+    private let buttonSignUp = UIButton()
+  
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initialize()
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        setupLayout()
+    }
+    
+    private func initialize() {
+        view.backgroundColor = UIColor.blueGreen
+        setupButtons()
+        setupLabels()
+    }
+    
+    struct Constants {
+        static let leading: CGFloat = 50
+        static let buttonHeight: CGFloat = 40
+        static let buttonWidth: CGFloat = 150
+        static let bottomOrTop: CGFloat = 70
+    }
+    
+    
+    private func setupButtons() {
+        
+        buttonContinue.setTitle("Register later", for: .normal)
+        buttonContinue.titleLabel?.numberOfLines = 0
+        buttonContinue.titleLabel?.font = UIFont(name: "Nunito-Regular", size: 14)
+        view.addSubview(buttonContinue)
+        
+        buttonContinue.addTarget(self, action: #selector(buttonContinueTapped), for: .touchUpInside)
+
+        buttonSignUp.backgroundColor = UIColor.honeyYellow
+        buttonSignUp.setTitle("Sign Up", for: .normal)
+        buttonSignUp.titleLabel?.font = UIFont(name: "Nunito-Regular", size: 20)
+        buttonSignUp.titleLabel?.adjustsFontSizeToFitWidth = true
+        buttonSignUp.layer.cornerRadius = 20
+        view.addSubview(buttonSignUp)
+       
+   //     buttonSignUp.addTarget(self, action: #selector(buttonSignUpTapped), for: .touchUpInside)
+    }
+    
+    private func setupLabels() {
+        
+        label.text = "🎉 Welcome! 🎉"
+        label.font = UIFont(name: "Nunito-Black", size: 26)
+        label.textColor = .white
+        label.adjustsFontSizeToFitWidth = true
+        view.addSubview(label)
+        
+       
+        extralabel.text = "We are happy to welcome you! Using our application, you can buy rare paintings that are shown at exhibitions. Register in the app to bid on the paintings you like."
+        extralabel.font = UIFont(name: "Nunito-Regular", size: 24)
+        extralabel.numberOfLines = 0
+        extralabel.textColor = .white
+        extralabel.adjustsFontSizeToFitWidth = true
+        view.addSubview(extralabel)
+        
+    }
+    
+    private func setupLayout() {
+        
+        buttonContinue.snp.makeConstraints { make in
+            make.bottom.equalToSuperview().inset(UIScreen.main.bounds.height/20)
+            make.leading.trailing.equalToSuperview().inset(Constants.leading)
+        }
+        
+        buttonSignUp.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.bottom.equalTo(buttonContinue).inset(Constants.bottomOrTop/2)
+            make.height.equalTo(Constants.buttonHeight)
+            make.width.equalTo(Constants.buttonWidth)
+        }
+        
+        label.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().inset(UIScreen.main.bounds.height/10)
+        }
+        
+        extralabel.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(Constants.leading)
+            make.top.equalTo(label).inset(Constants.bottomOrTop)
+            make.bottom.lessThanOrEqualTo(buttonSignUp).inset(Constants.bottomOrTop)
+            
+        }
+        
+    }
+    
+    
+//    @objc private func buttonSignUpTapped() {
+//        let signUpVC = SignUpViewController()
+//        guard let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate else { return }
+//        sceneDelegate.window?.rootViewController = signUpVC
+//        UserDefaults.standard.set(true, forKey: "isFirstStart")
+//    }
+
+    @objc private func buttonContinueTapped() {
+        let tabBarVC = TabBarViewController()
+        guard let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate else { return }
+        sceneDelegate.window?.rootViewController = tabBarVC
+        UserDefaults.standard.set(true, forKey: "isFirstStart")
+    }
+}
+
