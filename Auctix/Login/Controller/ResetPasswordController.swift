77cc719dc1ca609ef70090dfb8a407641f18ee4e
ShagMichail <shiga2001@mail.ru>
2021-11-08 13:43:27 +0300

    Добавил сообщение на почту о смене пароля и пару лэйблов

--- Auctix/Login/Controller/ResetPasswordController.swift
+++ Auctix/Login/Controller/ResetPasswordController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class ResetPasswordController: UIViewController {
     
@@ -14,8 +15,18 @@ class ResetPasswordController: UIViewController {
     private let iconImage = UIImageView(image: UIImage(named: "Image3"))
     
     private let auctixLabel = UILabel()
+
+    private let attentionLabel = UILabel()
     
-    private let emailTextField = CustomTextField(placeholder: "Email")
+    private let messageLabel = UILabel()
+    
+    private let emailTextField: CustomTextField = {
+        let tf = CustomTextField(placeholder: "email")
+        tf.returnKeyType = .done
+        tf.keyboardType = .emailAddress
+        tf.textContentType = .emailAddress
+        return tf
+    }()
     
     private let resetPasswordButton: AuthButton = {
         let button = AuthButton(type: .system)
@@ -33,6 +44,7 @@ class ResetPasswordController: UIViewController {
         return button
     }()
     
+    private let custumAlert = CustomAlert()
     //MARK: Lifecycle
     
     override func viewDidLoad() {
@@ -43,7 +55,18 @@ class ResetPasswordController: UIViewController {
     
     //MARK: Selectors
     @objc func handleResetPassword() {
-        
+        let email = emailTextField.text!
+        if (!email.isEmpty){
+            Auth.auth().sendPasswordReset(withEmail: email) { (error) in
+                if error == nil {
+                    self.navigationController?.popToRootViewController(animated: true)
+                }else{
+                    //если есть какая-то ошибка отправки сообщения
+                }
+            }
+        }else{
+            self.custumAlert.showAlert(title: "Error", message: "Not all fields were entered correctly", viewController: self)
+        }
     }
     
     @objc func handleDismissal() {
@@ -76,6 +99,23 @@ class ResetPasswordController: UIViewController {
         
         view.addSubview(stack)
         stack.anchor(top: auctixLabel.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 16, paddingLeft: 32, paddingRight: 32)
+        
+        view.addSubview(attentionLabel)
+        attentionLabel.text = "Attention!!!"
+        attentionLabel.textColor = UIColor.blueGreen
+        attentionLabel.centerX(inView: view)
+        attentionLabel.font = .systemFont(ofSize: 30)
+        attentionLabel.anchor(top: stack.bottomAnchor, paddingTop: 20)
+        
+        view.addSubview(messageLabel)
+        messageLabel.text = "After clicking on the button, you will receive a message by e-mail with a changed password. Don't forget to check your email!"
+        messageLabel.textColor = UIColor.lightCornflowerBlue
+        messageLabel.numberOfLines = 0
+        messageLabel.font = .systemFont(ofSize: 20)
+        messageLabel.centerX(inView: view)
+        //messageLabel.anchor(top: attentionLabel.bottomAnchor, paddingTop: 10)
+        messageLabel.anchor(top: attentionLabel.bottomAnchor, left: view.safeAreaLayoutGuide.leftAnchor, right: view.safeAreaLayoutGuide.rightAnchor, paddingLeft: 50, paddingRight: 30)
+
         
     }
 }
